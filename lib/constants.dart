8514aeae3dae822dade1450a1be085b885fa06f1
muhammednashat <mnashat.dev@gmail.com>
2025-10-06 11:02:22 +0300

    init

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-
-// const String dataKey = "data";
-// const String  branchKey = "branch";
-// const String  Key= "";
-// const String  = "";
-// const String  = "";
-// const String  = "";
--- lib/model/banks/bank.dart
+++ lib/model/banks/bank.dart
@@ -1,7 +1,6 @@
 
 
 import 'package:bank_system/model/accounts/account.dart';
-import 'package:bank_system/model/transaction.dart';
 import 'package:bank_system/util/validation_amount.dart';
 
 abstract class Bank with ValidationAmount {
